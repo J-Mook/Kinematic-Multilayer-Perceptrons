1e67238d6bc69a08906f0077647e69792ce668b3
J-Mook <jwonmook@gmail.com>
2021-10-18 10:45:46 +0900

    git update

--- code.py
+++ code.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import tqdm
+import math
 
 def main():
 
@@ -15,11 +16,11 @@ def main():
     print(dataset.x_data)
     print(dataset.y_data)
 
-    dataloader = DataLoader(dataset, batch_size=10, shuffle=True)
+    dataloader = DataLoader(dataset, batch_size=100, shuffle=True)
     model = Net()
-    # optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3) 
-    nb_epochs = 50
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    # optimizer = torch.optim.SGD(model.parameters(), lr=1e-3) 
+    nb_epochs = 100
 
     criterion = nn.MSELoss()
 
@@ -27,7 +28,8 @@ def main():
 
     n=len(dataloader)
 
-    for epoch in tqdm.tqdm(range(nb_epochs)):
+    # for epoch in tqdm.tqdm(range(nb_epochs)):
+    for epoch in range(nb_epochs):
         running_loss = 0
         for batch_idx, samples in enumerate(dataloader):
             x_train, y_train = samples
@@ -44,16 +46,15 @@ def main():
 
             running_loss += loss.item()
             # loss_graph.append(loss.item()) # 한 epoch에 모든 배치들에 대한 평균 loss 리스트에 담고,
-            # print('Epoch {:4d}/{} Batch {}/{} Cost: {:.6f}'.format(epoch, nb_epochs, batch_idx+1, len(dataloader),loss.item()))
-        
+        print('Epoch {:4d}/{} Batch {}/{} Cost: {:.6f}'.format(epoch, nb_epochs, batch_idx+1, len(dataloader),running_loss/n))
         loss_graph.append(running_loss/n) # 한 epoch에 모든 배치들에 대한 평균 loss 리스트에 담고,
     # plt.figure(figsize=(10,6))
     plt.plot(np.linspace(0,1,len(loss_graph)), loss_graph)
   
     plt.show()
 
-    new_var =  torch.FloatTensor([[1.5, 1.5]]) # -160.514349	274.7753481
-    pred_y = model(new_var) 
+    new_var =  torch.FloatTensor([[1.5 / (2*math.pi), 1.5 / (2*math.pi)]]) # -0.459627647	0.569307497
+    pred_y = model(new_var) * 2
     print("훈련 후 입력이 250, 180, 1.5, 1.5일 때의 예측값 :", pred_y) 
 
 # Dataset 상속
@@ -64,6 +65,12 @@ class CustomDataset(Dataset):
         print(df.shape)
         self.x_data = df.iloc[:, 2:-2].values
         self.y_data = df.iloc[:, -2:].values
+        print(self.x_data)
+        self.x_data = self.x_data / (2*math.pi)
+        self.y_data = self.y_data / 2
+        print(self.x_data)
+
+
 
     # 총 데이터의 개수를 리턴
     def __len__(self): 
@@ -78,23 +85,21 @@ class CustomDataset(Dataset):
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(2, 64)
-        self.fc2 = nn.Linear(64, 64)
-        self.fc3 = nn.Linear(64, 128)
-        self.fc4 = nn.Linear(128, 2)
-        # self.fc4 = nn.Linear(128, 128)
-        # self.fc5 = nn.Linear(128, 128)
-        # self.fc6 = nn.Linear(128, 2)
+        self.fc1 = nn.Linear(2, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 2)
+        # self.fc4 = nn.Linear(64, 2)
+        # self.fc5 = nn.Linear(8, 1)
+        # self.fc6 = nn.Linear(200, 2)
 
 
     def forward(self, x):
-        x = F.sigmoid(self.fc1(x))
-        x = F.sigmoid(self.fc2(x))
-        x = F.sigmoid(self.fc3(x))
-        # x = F.relu(self.fc4(x))
-        # x = F.relu(self.fc5(x))
-
-        x = F.sigmoid(self.fc4(x))
+        x = F.tanh(self.fc1(x))
+        x = F.tanh(self.fc2(x))
+        x = F.tanh(self.fc3(x))
+        # x = F.tanh(self.fc4(x))
+        # x = F.tanh(self.fc5(x))
+        # x = F.sigmoid(self.fc6(x))
 
         return x
 
